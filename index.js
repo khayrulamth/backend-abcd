0f339f7fcf4a9f633818a7eba2f41ffb6af7b66f
Md. Khayrul Islam <devkhayrul@gmail.com>
2021-08-01 15:56:55 +0600

    node intro with get & post method

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(cors());
+
+let jsonParser = bodyParser.json()
+
 
 app.get('/', (req, res) => {
-    res.send('thank you for creating me');
+        res.send("love you ");
+});
+
+
+const users = ['asad', 'mooin','kala', 'badal', 'saaj']
+
+app.get('/fruits/banana', (req, res) => {
+    res.send({ product: 'banana', price: 300, quantity: 45 });
 })
 
-app.listen(3000,()=>{
-    console.log('listening on port 3000');
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const name = users[userId];
+    res.send({userId,name});
+    console.log(name);
+})
+
+app.post('/addUser',jsonParser, (req, res) => {
+    const user = req.body;
+    user.id = 44;
+    res.send(user);
+})
+
+
+app.listen(3000, () => {
+    console.log('creating in port 3000');
 });
